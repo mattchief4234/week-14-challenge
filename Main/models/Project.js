798fb4c9535cddf248dcfef243bb6dd910c478bd
mattchief4234 <mattchief4234@gmail.com>
2023-02-08 18:39:52 -0700

    updated the code!
    IT WORKS!!!!!! I am so happy, now I just need to add in a few more things and figure out how to add a comment secction, then I think its done

--- Main/models/Project.js
+++ Main/models/Project.js
@@ -11,21 +11,17 @@ Project.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING ,
     },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
     },
     user_id: {
       type: DataTypes.INTEGER,
