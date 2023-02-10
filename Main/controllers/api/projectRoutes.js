7a0bffe5906a6865760cd9444ab24cc49cdb16a3
mattchief4234 <mattchief4234@gmail.com>
2023-02-09 22:23:20 -0700

    updated some miner things in the code

--- Main/controllers/api/projectRoutes.js
+++ Main/controllers/api/projectRoutes.js
@@ -5,8 +5,10 @@ const withAuth = require('../../utils/auth');
 router.post('/', withAuth, async (req, res) => {
   try {
     const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
+      where: {
+        id:req.body,
+        user_id: req.session.user_id,
+      }
     });
 
     res.status(200).json(newProject);
